2cd98c7267f70d2cd433905144b96d3218a63512
Tim Tsai <tim.tsai@xuenn.com>
2021-03-12 11:36:33 +0800

    Add high light js

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -19,3 +19,9 @@ export default {
   },
 };
 </script>
+<style scoped>
+textarea {
+  height: 100vh;
+  width: 100vh;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import marked from "marked";
+import hljs from 'highlight.js';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -14,12 +15,16 @@ export default new Vuex.Store({
     },
     actions: {
         update(context, e) {
-          context.commit("update", e.target.value);
+            context.commit("update", e.target.value);
         },
     },
     getters: {
         compiledMarkdown(state) {
-            return marked(state.editorText);
+            return marked(state.editorText, {
+                highlight: function(code, lang) {
+                    return hljs.highlight(lang, code).value;
+                },
+            });
         },
         getEditorText(state) {
             return state.editorText;
